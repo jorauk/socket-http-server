92b1d95c5328a6fc42c5b79d9cd0645bdc8afc98
Jonathan Mauk <jorauk@uw.edu>
2019-06-14 18:21:14 -0700

    Added response_method_not_allowed and parse_request code per lesson content.

--- http_server.py
+++ http_server.py
@@ -21,19 +21,19 @@ def response_ok(body=b"This is a minimal response", mimetype=b"text/plain"):
 
     return b"\r\n".join([
         b"HTTP/1.1 200 OK",
-        b"Content-Type: text/plain",
+        b"Content-Type: " + mimetype,
         b"",
-        b"Hello hello!",
-        b"Hello hello!",
-        b"",
-        b"Hello hello!",
+        body,
     ])
 
 def response_method_not_allowed():
     """Returns a 405 Method Not Allowed response"""
 
-    # TODO: Implement response_method_not_allowed
-    return b""
+    return b"\r\n".join([
+        b"HTTP/1.1 405 Method Not Allowed",
+        b"",
+        b"You can't do that on this server!"
+    ])
 
 
 def response_not_found():
@@ -51,8 +51,12 @@ def parse_request(request):
     NotImplementedError if the method of the request is not GET.
     """
 
-    # TODO: implement parse_request
-    return ""
+    method, path, version = request.split("\r\n")[0].split(" ")
+    
+    if method != "GET":
+        raise NotImplementedError
+        
+    return path
 
 def response_path(path):
     """
@@ -124,21 +128,24 @@ def server(log_buffer=sys.stderr):
 		
 
                 print("Request received:\n{}\n\n".format(request))
+                
+                try:
+                    path = parse_request(request)
 
-                # TODO: Use parse_request to retrieve the path from the request.
+                    # TODO: Use response_path to retrieve the content and the mimetype,
+                    # based on the request path.
 
-                # TODO: Use response_path to retrieve the content and the mimetype,
-                # based on the request path.
-
-                # TODO; If parse_request raised a NotImplementedError, then let
-                # response be a method_not_allowed response. If response_path raised
-                # a NameError, then let response be a not_found response. Else,
-                # use the content and mimetype from response_path to build a 
-                # response_ok.
-                response = response_ok(
-                    body=b"Welcome to my web server",
-                    mimetype=b"text/plain"
-                )
+                    # TODO; If parse_request raised a NotImplementedError, then let
+                    # response be a method_not_allowed response. If response_path raised
+                    # a NameError, then let response be a not_found response. Else,
+                    # use the content and mimetype from response_path to build a 
+                    # response_ok.
+                    response = response_ok(
+                        body=b"Welcome to my web server",
+                        mimetype=b"text/plain"
+                    )
+                except NotImplementedError:
+                    response = response_method_not_allowed()
 
                 conn.sendall(response)
             except:
